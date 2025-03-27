2f50346a7d94c4efe22dffd94313bf0ecc6a313f
Maurício Dias de Carvalho Machado <mauricio.machado@antt.gov.br>
2025-03-27 12:43:20 -0300

    Ajuste na configuração do Swagger

--- ComicTracker.API/Program.cs
+++ ComicTracker.API/Program.cs
@@ -5,6 +5,7 @@ using ComicTracker.Infrastructure.Data;
 using ComicTracker.Infrastructure.Repositories;
 using ComicTracker.Infrastructure.Services.ComicVine;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,9 +23,23 @@ builder.Services.AddScoped<IPublisherRepository, PublisherRepository>();
 builder.Services.AddScoped<IComicVineService, ComicVineService>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Configuração do Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ComicTracker API",
+        Version = "v1",
+        Description = "API para gerenciamento de coleções de HQs",
+        Contact = new OpenApiContact
+        {
+            Name = "Maurício Oliveira",
+            Email = "mauridf@gmail.com"
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -32,7 +47,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "ComicTracker API V1");
+    });
 }
 
 app.UseHttpsRedirection();
